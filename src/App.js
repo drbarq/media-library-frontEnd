b35cc7f2268c37a94d1389bdf3e8e4bf36a7fe90
drbarq <J.Tustin@gmail.com>
2019-07-10 13:56:54 -0600

    added home page fetching from backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,  {Component} from 'react';
 import './App.css';
+// import PodcastPlane from "./components/PodcastPlane"
+const groupPodcastURL = "https://secret-gorge-82811.herokuapp.com/groups/1/podcasts"
+const groupUsers = "https://secret-gorge-82811.herokuapp.com/groups/1/users"
+// const baseURL = "https://secret-gorge-82811.herokuapp.com/"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      teamSelection: '',
+      podcasts: [],
+      teamUsers: [],
+      newPodcast: {
+        firstName: "",
+        lastName: "",
+        role: "student"
+    }
+    }
+  }
+
+  componentDidMount() {
+    fetch(groupPodcastURL)
+      .then(results => results.json())
+      .then(podcasts => this.setState({podcasts}))
+      .catch(error => console.error(error))
+    fetch(groupUsers)
+      .then(results => results.json())
+      .then(teamUsers => this.setState({teamUsers}))
+      .catch(error => console.error(error))
+  }
+
+  updateFilterCategory = event => {
+    this.setState({
+      teamSelection: event.target.value
+    })
+  }
+
+  displayedPodCasts = () => {
+    return this.state.podcasts
+  }
+
+  teamUsers = () => {
+    return this.state.teamUsers
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <h1>The Media Library</h1>
+            <div className="team-and-user-control">
+              {/* <Search searchTerm={this.state.searchTerm} updateSearchTerm={this.updateSearchTerm}/> */}
+              <form className="Team-Selection">
+                <select name="filter-team" onChange={this.updateFilterCategory} >
+                  <option value="selectTeam">Please Select a Team</option>
+                  <option value="Team1">Show Only Team 1</option>
+                  <option value="Team2">Show Only Team2</option>
+                </select>
+              </form >
+              <div className="team-user-info">
+                <h3>Team Stats</h3>
+                <p>Number of Members: {this.state.teamUsers.length}</p>
+                <p>Number of Podcasts Submitted: {this.state.podcasts.length}</p>
+              </div>
+              <div className="user-info">
+                <h4>User Information</h4>
+              </div>
+            </div>  
+        </header>
+        <table className="podcast-table">
+            <thead>
+              <tr>
+                <th>Show Name</th>
+                <th>Episode Name</th>
+                <th>User Comments</th>
+                <th>URL</th>
+                <th>Submitted By:</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.displayedPodCasts().map(podcast => {
+                  return(
+                    <tr key={podcast.id}>
+                      <td>{podcast.showName}</td>
+                      <td>{podcast.episodeName}</td>
+                      <td>{podcast.comment}</td>
+                      <td>{podcast.url}</td>
+                      <td>{podcast.user_id}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+          
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/PodcastPlane.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class PodcastPlane extends Component {
+    render(){
+        return(
+            <h1>What up </h1>
+        )
+    }
+}
